ee14ac8b3cd4205c897d64b8e3e630e34144dbe5
Elizaveta Agey <e.agey@funcount.com>
2022-10-20 12:08:15 +0700

    init: repository craeation

---
+++ freecodecamp.js
@@ -0,0 +1,16 @@
+// JavaScript Algorithms and Data Structures
+
+//-----TASK
+//Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+function sum(arr, n) {
+
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
+    }
+
+}
+
+//-----TASK

ef5807b7958a6ab9e9759834f28799bff56809ca
Liza Agey <agey.eliz@gmail.com>
2022-11-27 23:12:21 +0700

    feat: if-statements are trained

--- if-statements.js
+++ if-statements.js
@@ -66,3 +66,32 @@ function checkSign(num) {
 }
 
 checkSign(10);
+
+// CODEWARS
+
+//-----TASK
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!//
+// The function takes a name as its only argument, and returns one of the following strings://
+//     name + " plays banjo"
+//     name + " does not play banjo"
+// Names given are always valid strings.
+
+
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0).toLowerCase() === "r") {
+        return name + " plays banjo"
+    } else {
+        return name + " does not play banjo"
+    }
+
+}
+
+//-----TASK
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates
+// and the paperwork has 'm' pages.//
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+function paperwork(n, m) {
+    if ((n <0 || m<0 ) ){return 0} else {return m * n}
+
+}
